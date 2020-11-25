3c6d0fd22419cb52e92d8f981ded3000c53b4a64
PyryQ <73101045+PyryQ@users.noreply.github.com>
2020-11-25 13:51:37 +0200

    serveriä päiviettty

--- server/index.js
+++ server/index.js
@@ -3,7 +3,19 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! GET')
+})
+
+app.post('/', (req, res) => {
+  res.send('Hello World! POST')
+})
+
+app.delete('/', (req, res) => {
+  res.send('Hello World! DELETE')
+})
+
+app.put('/', (req, res) => {
+  res.send('Hello World! PUT')
 })
 
 app.listen(port, () => {

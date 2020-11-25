0b66e28579e2ec69af67d752e887c6f6b0f9f25b
PyryQ <73101045+PyryQ@users.noreply.github.com>
2020-11-25 13:32:50 +0200

    kansiot päivettty

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
-var express = require('express');
+const express = require('express')
+const app = express()
+const port = 4000
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/', function (req,res){
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })

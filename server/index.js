c5b8e403006bbe4bdbde3d4e41f438d18c01d370
PyryQ <73101045+PyryQ@users.noreply.github.com>
2020-12-01 10:34:15 +0200

    serverin pyyntöjä päivitetty

--- server/index.js
+++ server/index.js
@@ -12,8 +12,68 @@ app.use(cors())
 
 const db = require('./db')
 
-//cascade
-//https://kb.objectrocket.com/postgresql/how-to-use-the-postgresql-delete-cascade-1369#:~:text=In%20PostgreSQL%2C%20a%20cascade%20means,key%20relationship%20is%20in%20place
+
+///////////////////////////////POST
+
+//Lisää tentti (lisää defaultarvot tietokantaan?)
+app.post('/lisaatentti/:nimi/:tp/:mp/:ta/:tl/:pr', (req, res, next) => {
+  db.query("INSERT INTO tentti (nimi, tenttipisteet, minimipisteet, tentin_aloitusaika, tentin_lopetusaika, pisterajat) values ($1, $2, $3, $4, $5, $6);", 
+  [req.params.nimi, req.params.tp, req.params.mp, req.params.ta, req.params.tl, req.params.pr], (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.send("Tentin lisäys onnistui")
+  })
+})
+//http://localhost:4000/lisaatentti/TenttiNimi/30/10/2021-01-01 12:00:00/2021-01-01 14:00:00/pisterajat
+
+//Lisää kysymys
+app.post('/lisaakysymys/:tentti_id/:kysymys/:pisteet/', (req, res, next) => {
+  db.query("INSERT INTO kysymys (tentti_id_fk, kysymys, kysymyspisteet) VALUES ($1, $2, $3);", 
+  [req.params.tentti_id, req.params.kysymys, req.params.pisteet], (err, result) => { 
+    if (err) {
+      return next(err)
+    }
+    res.send("Kysymyksen lisäys onnistui")
+  })
+
+})
+
+//Lisää vastaus
+app.post('/lisaavastaus/:kysymys_id/:vastaus/:oikea_vastaus', (req, res, next) => {
+  db.query("INSERT INTO vastaus (kysymys_id_fk, vastaus, oikea_vastaus) VALUES ($1, $2, $3);", 
+  [req.params.kysymys_id, req.params.vastaus, req.params.oikea_vastaus], (err, result) => { 
+    if (err) {
+      return next(err)
+    }
+    res.send("Vastauksen lisäys onnistui")
+  })
+})
+
+//Lisää käyttäjä
+app.post('/lisaakayttaja/:en/:sn/:sp/:r/:ss', (req, res, next) => {
+  db.query("INSERT INTO käyttäjä (etunimi, sukunimi, sähköposti, rooli, salasana) values ($1, $2, $3, $4, $5);", 
+  [req.params.en, req.params.sn, req.params.sp, req.params.r, req.params.ss], (err, result) => { 
+    if (err) {
+      return next(err)
+    }
+    res.send(result) 
+  }) 
+})
+//http://localhost:4000/lisaakayttaja/testi/testinen/testaus/kayttaja/salasana12345
+
+//Lisää käyttäjän vastaus
+app.post('/lisaakayttajanvastaus/:k_id/:v_id/:k_valinta/:v_oikein', (req, res, next) => {
+  db.query("INSERT INTO käyttäjänvastaus (käyttäjä_id_fk, vastaus_id_fk, käyttäjän_valinta, vastaus_oikein) VALUES ($1, $2, $3, $4);", 
+  [req.params.k_id, req.params.v_id, req.params.k_valinta, req.params.v_oikein], (err, result) => { 
+    if (err) {
+      return next(err)
+    }
+    res.send("onnistui")
+  }) 
+})
+
+
 
 
 ////////////////////////////////////// GET
@@ -28,6 +88,7 @@ app.get('/tentti/:id', (req, res, next) => {
   })
 })
 
+//Kaikki tentit
 app.get('/tentit', (req, res, next) => {
   db.query('SELECT * FROM tentti', (err, result) => {
     if (err) {
@@ -80,9 +141,9 @@ app.get('/kayttajat', (req, res, next) => {
 })
 
 //Käyttäjän tentit
-app.get('/kayttajantentit/:sahkoposti', (req, res, next) => {
-  db.query('SELECT * FROM käyttäjäntentti WHERE käyttäjä_id_fk = (SELECT käyttäjä_id FROM käyttäjä WHERE sähköposti=$1)', 
-  [req.params.sahkoposti], (err, result) => {
+app.get('/kayttajantentit/:kayttaja_id', (req, res, next) => {
+  db.query('SELECT * FROM käyttäjäntentti WHERE käyttäjä_id_fk = $1', 
+  [req.params.kayttaja_id], (err, result) => {
     if (err) {
       return next(err)
     }
@@ -90,10 +151,10 @@ app.get('/kayttajantentit/:sahkoposti', (req, res, next) => {
   })
 })
 
-//Käyttäjän vastaukset
-app.get('/kayttajanvastaukset/:sahkoposti', (req, res, next) => {
-  db.query('SELECT * FROM käyttäjänvastaus WHERE käyttäjä_id_fk = (SELECT käyttäjä_id FROM käyttäjä WHERE sähköposti=$1)', 
-  [req.params.sahkoposti], (err, result) => {
+//Käyttäjän tentin vastaukset
+app.get('/kayttajanvastaukset/:kayttaja_id/:vastaus_id', (req, res, next) => {
+  db.query('SELECT * FROM käyttäjänvastaus WHERE käyttäjä_id_fk = $1 AND vastaus_id_fk = $2', 
+  [req.params.kayttaja_id, req.params.vastaus_id], (err, result) => {
     if (err) {
       return next(err)
     }
@@ -108,73 +169,10 @@ app.get('/kayttajanrooli/:sahkoposti', (req, res, next) => {
     if (err) {
       return next(err)
     }
-    res.send(result.rows[0])
+    res.send(result.rows)
   })
 })
 
-
-
-
-
-///////////////////////////////POST
-
-//lisää tentti
-app.post('/lisaatentti/:nimi/:tp/:mp/:ta/:tl/:pr', (req, res, next) => {
-  db.query("INSERT INTO tentti (nimi, tenttipisteet, minimipisteet, tentin_aloitusaika, tentin_lopetusaika, pisterajat) values ($1, $2, $3, $4, $5, $6);", 
-  [req.params.nimi, req.params.tp, req.params.mp, req.params.ta, req.params.tl, req.params.pr], (err, result) => {
-    if (err) {
-      return next(err)
-    }
-    res.send("Tentin lisäys onnistui")
-  })
-})
-//http://localhost:4000/lisaatentti/TenttiNimi/30/10/2021-01-01 12:00:00/2021-01-01 14:00:00/pisterajat
-
-//lisää kysymys
-app.post('/lisaakysymys/:tentti_id/:kysymys/:pisteet/', (req, res, next) => {
-  db.query("INSERT INTO kysymys (tentti_id_fk, kysymys, kysymyspisteet) VALUES ($1, $2, $3);", 
-  [req.params.tentti_id, req.params.kysymys, req.params.pisteet], (err, result) => { 
-    if (err) {
-      return next(err)
-    }
-    res.send("Kysymyksen lisäys onnistui")
-  })
-
-})
-
-//lisää vastaus
-app.post('/lisaavastaus/:kysymys_id/:vastaus/:oikea_vastaus', (req, res, next) => {
-  db.query("INSERT INTO vastaus (kysymys_id_fk, vastaus, oikea_vastaus) VALUES ($1, $2, $3);", 
-  [req.params.kysymys_id, req.params.vastaus, req.params.oikea_vastaus], (err, result) => { 
-    if (err) {
-      return next(err)
-    }
-    res.send("Vastauksen lisäys onnistui")
-  })
-})
-
-//Lisää käyttäjä
-app.post('/lisaakayttja/:en/:sn/:sp/:r/:ss', (req, res, next) => {
-  db.query("INSERT INTO käyttäjä (etunimi, sukunimi, sähköposti, rooli, salasana) values ($1, $2, $3, $4, $5);", 
-  [req.params.en, req.params.sn, req.params.sp, req.params.r, req.params.ss], (err, result) => { 
-    if (err) {
-      return next(err)
-    }
-    res.send(result) 
-  }) 
-})
-//http://localhost:4000/lisaakayttaja/testi/testinen/testaus/kayttaja/salasana12345
-
-//Lisää käyttäjän vastaus
-app.post('/lisaakayttajanvastaus/:k-sp/:vastaus/:k_valinta/:v_oikein', (req, res, next) => {
-  db.query("INSERT INTO käyttäjänvastaus (käyttäjä_id_fk, vastaus_id_fk, käyttäjän_valinta, vastaus_oikein) VALUES ((SELECT käyttäjä_id_id FROM käyttäjä WHERE sähköposti='testi@testi.fi'), (SELECT vastaus_id FROM vastaus WHERE vastaus='Testivastaus 1'), $3, $4);", 
-  [req.params.k-sp, req.params.vastaus, req.params.k_valinta, req.params.v_oikein], (err, result) => { 
-    if (err) {
-      return next(err)
-    }
-    res.send("onnistui")
-  }) 
-})
 
 
 
@@ -189,33 +187,43 @@ app.put('/paivitatentti/:tentti_id/:uusinimi/:tp/:mp/:ta/:tl/:pr', (req, res, ne
     if (err) {
       return next(err)
     }
-    res.send(result.row)
+    res.send("Tentin päivitys onnistui")
   })
 })
 //http://localhost:4000/paivitatentti/Merkkitentti/Merkkitentti päivitetty/30/10/2021-03-03 20:00:00/2021-03-03 22:00:00/pisterajat
 
 //päivitä kysymys
-app.post('/paivitakysymys/:tentti_id/:uusiKysymys/:pisteet/', (req, res, next) => {
-  db.query("INSERT INTO kysymys kysymys = $2, kysymyspisteet = 3$ WHERE tentti_id_fk = $1;", 
-  [req.params.uusiKysymys, req.params.pisteet, req.params.tentti_nimi], (err, result) => { 
+app.put('/paivitakysymys/:tentti_id/:uusiKysymys/:pisteet/', (req, res, next) => {
+  db.query("UPDATE kysymys SET kysymys = $2, kysymyspisteet = 3$ WHERE tentti_id_fk = $1;", 
+  [req.params.tentti_id, req.params.uusiKysymys, req.params.pisteet], (err, result) => { 
     if (err) {
       return next(err)
     }
-    res.send(result)
+    res.send("Kysymyksen päivitys onnistui")
   })
 })
 
-//lisää vastaus
-app.post('/lisaavastaus/:kysymys_id/:vastaus/:oikea_vastaus', (req, res, next) => {
-  db.query("INSERT INTO vastaus (vastaus, oikea_vastaus, kysymys_id_fk) VALUES ($1, $2, $3);", 
-  [req.params.kysymys_id, req.params.vastaus, req.params.oikea_vastaus], (err, result) => { 
+//päivitä vastaus
+app.put('/paivitavastaus/:kysymys_id/:vastaus/:oikea_vastaus', (req, res, next) => {
+  db.query("UPDATE vastaus SET vastaus = $2, oikea_vastaus = 3$ WHERE kysymys_id_fk = $1;", 
+  [req.params.tentti_id, req.params.uusiKysymys, req.params.pisteet], (err, result) => { 
     if (err) {
       return next(err)
     }
-    res.send()
+    res.send("Vastausksen päivitys onnistui")
   })
 })
 
+//päivitä käyttäjän vastaus
+app.put('/paivitakayttajanvastaus/:k_id/:v_id/:k_valinta/:v_oikein', (req, res, next) => {
+  db.query("UPDATE käyttäjänvastaus SET käyttäjän_valinta = $3, vastaus_oikein = $4 WHERE käyttäjä_id_fk = $1 AND vastaus_id_fk = $2;", 
+  [req.params.k_id, req.params.v_id, req.params.k_valinta, req.params.v_oikein], (err, result) => { 
+    if (err) {
+      return next(err)
+    }
+    res.send("Käyttäjän vastauksen päivitys onnistui")
+  }) 
+})
 
 
 
@@ -240,8 +248,8 @@ app.delete('/poistatentti/:tentti_id', (req, res, next) => {
   })
 })
 
-app.delete('/poistakysymys/:kysymys', (req, res, next) => {
-  db.query("DELETE FROM kysymys WHERE kysymys=$1;", [req.params.kysymys], (err, result) => {
+app.delete('/poistakysymys/:kysymys_id', (req, res, next) => {
+  db.query("DELETE FROM kysymys WHERE kysymys_id=$1;", [req.params.kysymys_id], (err, result) => {
     if (err) {
       return next(err)
     }
@@ -249,8 +257,8 @@ app.delete('/poistakysymys/:kysymys', (req, res, next) => {
   })
 })
 
-app.delete('/poistavastaus/:vastaus', (req, res, next) => {
-  db.query("DELETE FROM vastaus WHERE vastaus=$1;", [req.params.vastaus], (err, result) => {
+app.delete('/poistavastaus/:vastaus_id', (req, res, next) => {
+  db.query("DELETE FROM vastaus WHERE vastaus_id =$1;", [req.params.vastaus_id], (err, result) => {
     if (err) {
       return next(err)
     }
@@ -258,9 +266,9 @@ app.delete('/poistavastaus/:vastaus', (req, res, next) => {
   })
 })
 
-app.delete('/poistakayttajantentti/:tentti_nimi/:sahkoposti', (req, res, next) => {
-  db.query("DELETE FROM kayttajantentti WHERE tentti_id_fk = (SELECT tentti_id FROM tentti WHERE nimi=$1) AND käyttäjä_id_fk = (SELECT käyttäjä_id FROM käyttäjä WHERE sähköposti=$2);", 
-  [req.params.tentti_nimi, req.params.sahkoposti], (err, result) => {
+app.delete('/poistakayttajantentti/:tentti_id/:kayttaja_id', (req, res, next) => {
+  db.query("DELETE FROM kayttajantentti WHERE tentti_id_fk = $1 AND käyttäjä_id_fk = $2;", 
+  [req.params.tentti_id, req.params.kayttaja_id], (err, result) => {
     if (err) {
       return next(err)
     }
@@ -268,9 +276,9 @@ app.delete('/poistakayttajantentti/:tentti_nimi/:sahkoposti', (req, res, next) =
   })
 })
 
-app.delete('/poistakayttajanvastaus/:vastaus/:sahkoposti', (req, res, next) => {
-  db.query("DELETE FROM vastaus WHERE vastaus_id_fk = (SELECT vastaus_id FROM vastaus WHERE vastaus=$1) AND käyttäjä_id_fk = (SELECT käyttäjä_id FROM käyttäjä WHERE sähköposti=$2);", 
-  [req.params.vastaus, req.params.sahkoposti], (err, result) => {
+app.delete('/poistakayttajanvastaus/:kayttaja_id/:vastaus_id', (req, res, next) => {
+  db.query("DELETE FROM vastaus WHERE käyttäjä_id_fk = $2 AND vastaus_id_fk = $1;", 
+  [req.params.kayttaja_id, req.params.vastaus_id], (err, result) => {
     if (err) {
       return next(err)
     }

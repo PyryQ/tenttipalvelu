7bced0733cfa7092a73f1d356e73cccb19bf9eb4
PyryQ <73101045+PyryQ@users.noreply.github.com>
2021-01-29 12:56:53 +0200

    c#-testi

---
+++ client/Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Npgsql;
+
+namespace Version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = "Host=localhost;Username=postgres;Password=MnoP1994;Database=Tenttikanta";
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "SELECT version()";
+
+            using var cmd = new NpgsqlCommand(sql, con);
+
+            var version = cmd.ExecuteScalar().ToString();
+            Console.WriteLine($"PostgreSQL version: {version}");
+        }
+    }
+}    

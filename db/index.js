8bc804c7c5fcfac8e9d371870deee3d7735ab99e
PyryQ <73101045+PyryQ@users.noreply.github.com>
2021-02-01 10:34:14 +0200

    build

--- db/index.js
+++ db/index.js
@@ -4,10 +4,10 @@ var connectInfo = {};
 
 if(process.env.HEROKU){
   connectInfo = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Tenttikanta',
-    password: 'MnoP1994',
+    user: 'xsztrqfjstsozv',
+    host: 'ec2-3-248-4-172.eu-west-1.compute.amazonaws.com',
+    database: 'd1i1949tdgr30l',
+    password: '2b23c0f774378c5ea8b5fe2257c56d6eb651d2dfc45a42f23dff7e5a454eb66b',
     port: 5432
   }
 }

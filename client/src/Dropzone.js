bf6ff85660c366dee3660155f7d119ce2b877964
PyryQ <73101045+PyryQ@users.noreply.github.com>
2021-02-02 12:33:30 +0200

    build

--- client/src/Dropzone.js
+++ client/src/Dropzone.js
@@ -1,72 +1,72 @@
-import {useDropzone} from 'react-dropzone';
-import { useCallback } from 'react'
-import './App.css';
-import Card from '@material-ui/core/Card';
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
+// import {useDropzone} from 'react-dropzone';
+// import { useCallback } from 'react'
+// import './App.css';
+// import Card from '@material-ui/core/Card';
+// import axios from 'axios';
+// import Button from '@material-ui/core/Button';
 
 
 
-//Superagent
-const superagent = require('superagent');
+// //Superagent
+// const superagent = require('superagent');
  
-// callback
-superagent
-  .post('/api/pet')
-  .send({ name: 'Manny', species: 'cat' }) // sends a JSON post body
-  .set('X-API-Key', 'foobar')
-  .set('accept', 'json')
-  .end((err, res) => {
-    // Calling the end function will send the request
-  });
+// // callback
+// superagent
+//   .post('/api/pet')
+//   .send({ name: 'Manny', species: 'cat' }) // sends a JSON post body
+//   .set('X-API-Key', 'foobar')
+//   .set('accept', 'json')
+//   .end((err, res) => {
+//     // Calling the end function will send the request
+//   });
  
-// promise with then/catch
-superagent.post('/api/pet').then(console.log).catch(console.error);
+// // promise with then/catch
+// superagent.post('/api/pet').then(console.log).catch(console.error);
   
   
-  //dropzone
-  export default function Dropzone(props) {
+//   //dropzone
+//   export default function Dropzone(props) {
 
     
-    const onDrop = useCallback(async files => {
-        console.log(files);
+//     const onDrop = useCallback(async files => {
+//         console.log(files);
     
-        const req = await axios.post('http://localhost:4000/upload');
+//         const req = await axios.post('http://localhost:4000/upload');
     
-        files.forEach(file => {
-          req.attach('file', file);
-        });
-        req.end((err, res) => {
-          console.log(res)
-        });
+//         files.forEach(file => {
+//           req.attach('file', file);
+//         });
+//         req.end((err, res) => {
+//           console.log(res)
+//         });
     
-      }, []);
+//       }, []);
 
-      const {acceptedFiles, getRootProps, getInputProps} = useDropzone({onDrop});
+//       const {acceptedFiles, getRootProps, getInputProps} = useDropzone({onDrop});
 
-      const files = acceptedFiles.map(file => (
-        <li key={file.path}>
-          {file.path} - {file.size} bytes
-        </li>
-      ));
+//       const files = acceptedFiles.map(file => (
+//         <li key={file.path}>
+//           {file.path} - {file.size} bytes
+//         </li>
+//       ));
 
-        //Anssin linkkaama pätkä
-      //let url = file && URL.createObjectURL(file)
-      //<img src={url} width={150} height={150} />
+//         //Anssin linkkaama pätkä
+//       //let url = file && URL.createObjectURL(file)
+//       //<img src={url} width={150} height={150} />
 
 
   
-    return (
-      <Card className="dropcontainer">
-        <div {...getRootProps({className: 'dropzone'})}>
-          <input {...getInputProps()} />
-          <p>Lisää vastaustiedostosi tähän.</p>
-        </div>
-        <aside>
-          <h4 className="files">Files</h4>
-          <ul>{files}</ul>
-        </aside>
-      </Card>
+//     return (
+//       <Card className="dropcontainer">
+//         <div {...getRootProps({className: 'dropzone'})}>
+//           <input {...getInputProps()} />
+//           <p>Lisää vastaustiedostosi tähän.</p>
+//         </div>
+//         <aside>
+//           <h4 className="files">Files</h4>
+//           <ul>{files}</ul>
+//         </aside>
+//       </Card>
       
-    );
-  }
+//     );
+//   }

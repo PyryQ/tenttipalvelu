54210de3e552866eb613903d79b1dbd9cc5d6502
PyryQ <73101045+PyryQ@users.noreply.github.com>
2021-02-02 12:53:09 +0200

    app

--- index.js
+++ index.js
@@ -485,7 +485,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname +'/client/build/index.html'))
 })
 
-httpServer.listen(port, () => {
+app.listen(port, () => {
   console.log("Palvelin käynnistyi portissa: " + port)
 })
 
